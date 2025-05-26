a561970a923349297c231683b565177f7166c408
tom-moosbrugger <tamoosbrugger@gmail.com>
2025-05-26 10:28:41 -0700

    removed unused variables and unneeded comments

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,16 @@
 "use client";
 
 import { useState, useEffect, useRef } from "react";
-import Link from "next/link";
 import NavLink from "./NavLink";
 import ThemeToggle from "./ThemeToggle";
 import { Bars3Icon } from "@heroicons/react/24/solid";
 
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
+  // create DOM reference for the dropdown menu
   const menuRef = useRef<HTMLDivElement>(null)
 
+  // callback function for useEffect event listener
   const handleOutsideClick = (event: MouseEvent) => {
     // if the event target is not a child of the menuRef we created, close the menu
     if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
@@ -17,6 +18,7 @@ const Navbar = () => {
       }
   };
 
+  // add event listener to listen for clicks outside the dropdown menu
   useEffect(() => {
     document.addEventListener("click", handleOutsideClick);
 
@@ -35,16 +37,13 @@ const Navbar = () => {
         <ThemeToggle />
         {showMenu && (
           <div className="absolute top-8 right-12 flex flex-col items-center border-1 p-2 gap-2 min-w-42">
-            <div className="w-full border-b-1 text-center pb-2">
-              {/* <Link href="/">Projects</Link> */}
+            <div>
               <NavLink hRef="/" linkText="Projects" />
             </div>
             <div className="w-full border-b-1 text-center pb-2">
-              {/* <Link href="/">Resume</Link> */}
               <NavLink hRef="/" linkText="Resume" />
             </div>
             <div>
-              {/* <Link href="/">Contact</Link> */}
               <NavLink hRef="/" linkText="Contact" />
             </div>
           </div>
@@ -62,19 +61,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
-/* 
-
-we need a dropdown menu to show on smaller screens:
-
-goal is to have an icon that, when clicked, displays the NavLinks and ThemeToggle in a dropdown
-when a link is clicked, it should navigate to the link and also close the menu
-when the themetoggle is clicked, it should toggle the theme and close the menu
-also, the menu should close if anywhere else on the page is clicked
-
-first, we need a div that holds all the links and the toggle
-second, we need a slice of state to determine if menu is shown
-third, we need a useEffect to create an event listener that listens for clicks elsewhere on the screen
-
-
-*/

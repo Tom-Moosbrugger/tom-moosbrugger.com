ded9587b2c008f47c638efa64b81e3b0dd51a923
tom-moosbrugger <tamoosbrugger@gmail.com>
2025-05-28 08:50:46 -0700

    changed navbar padding

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ import ThemeToggle from "@/components/Navbar/ThemeToggle";
 
 const Navbar = () => {
   return (
-    <nav className="flex flex-row justify-between items-center pl-2 pr-6 py-4 border-b-1">
+    <nav className="flex flex-row justify-between items-center pl-4 pr-4 py-4 border-b-1">
       <NavLink hRef="/" linkText="TomMoosbrugger" />
       <div className="flex gap-2 sm:gap-6">
         {/* dropdown menu for mobile */}

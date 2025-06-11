f8b49786a53b6f1ae5dbda761df3d1a3ff6c3ea0
tom-moosbrugger <tamoosbrugger@gmail.com>
2025-06-11 11:44:55 -0700

    added intersection observer to contact banner to dynamically add animation

--- components/HomePage/ContactBanner/ContactBanner.tsx
+++ components/HomePage/ContactBanner/ContactBanner.tsx
@@ -1,8 +1,41 @@
+"use client";
+
+import { useRef, useEffect } from "react";
+
 const ContactBanner = () => {
+  const bannerRef = useRef<HTMLElement | null>(null);
+
+  useEffect(() => {
+    // create new intersection observer that watches for an element entering the viewport
+    // once the element enters the viewport, add my custom animation utility class to its class
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("animate-slide-right")
+        }
+      });
+    }, { threshold: 0 });
+
+    const bannerNode = bannerRef.current;
+
+    if (bannerNode) {
+      observer.observe(bannerNode);
+    }
+
+    () => observer.disconnect();
+  }, []);
+
   return (
-    <section className="flex flex-col gap-6 items-center justify-center w-full bg-blue dark:bg-green dark:text-black border-y-2 dark:border-white p-10 m-10 animate-slide-right">
-      <p className="text-xl sm:text-4xl font-extrabold">What are you waiting for?</p>
-      <button className="text-xl sm:text-2xl border p-4 cursor-pointer hover:bg-white">Contact Me</button>
+    <section
+      ref={bannerRef}
+      className="flex flex-col gap-6 items-center justify-center w-full bg-blue dark:bg-green dark:text-black border-y-2 dark:border-white p-10 m-10"
+    >
+      <p className="text-xl sm:text-4xl font-extrabold">
+        What are you waiting for?
+      </p>
+      <button className="text-xl sm:text-2xl border p-4 cursor-pointer hover:bg-white">
+        Contact Me
+      </button>
     </section>
   );
 };

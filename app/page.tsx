78acea1b831701e08a11e5582c4349778988afe6
tom-moosbrugger <tamoosbrugger@gmail.com>
2025-06-11 17:16:26 -0700

    removed home page pseudocode

--- app/page.tsx
+++ app/page.tsx
@@ -15,30 +15,3 @@ const Home = () => {
 }
 
 export default Home;
-
-/* 
-
-Home page needs the following sections:
-
-Welcome:
-
-  Quick intro with introductory paragraph about me and photo.
-  Make sure to have different layouts for screen sizes
-  also include animation with typing
-
-Technologies and Skills
-  List of technologies and skills
-  Probably logo based
-  Different grid layouts depending on screen size
-
-Featured Projects
-  Quick list of featured projects?
-
-Personal Interests
-  Section with photos and animated gifs highlighting my personal interests: running, reading, traveling
-  Could be it's own page?
-
-
-At the bottom: link to contact form
-
-*/

8336dad8e66d74cfc981fc6afb1efe4fe5008e91
tom-moosbrugger <tamoosbrugger@gmail.com>
2025-06-13 14:01:05 -0700

    finished basic contact form layout

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,48 @@
+'use client';
+
+import ContactForm from '@/components/ContactPage/ContactForm';
+import ComponentElement from '@/components/Elements/ComponentElement';
+import { useState } from 'react';
+
 const Contact = () => {
-    return (
-      <h1>Content Forthcoming</h1>
-    )
+  const [tab, setTab] = useState('tab1');
+
+  const tabClass = 'border bg-';
+
+  return (
+    <main className="flex justify-center px-20 py-10">
+      <article className="rounded-lg border w-full min-w-sm max-w-4xl bg-blue dark:bg-green text-black">
+        <header className="flex flex-row items-center">
+          <h2
+            onClick={() => setTab('tab1')}
+            className={`text-shadow-light box-content py-3 flex flex-1 cursor-pointer items-center justify-center rounded-tl-lg text-xl font-bold sm:text-3xl ${tab === 'tab1' ? '' : 'bg-lightblue border-r border-b opacity-30'}`}
+          >
+            Contact Form
+            <br />
+          </h2>
+          <h2
+            onClick={() => setTab('tab2')}
+            className={`text-shadow-light box-content py-3 flex flex-1 cursor-pointer items-center justify-center rounded-tr-lg text-xl font-bold sm:text-3xl ${tab === 'tab2' ? '' : 'bg-lightblue border-b border-l opacity-30'}`}
+          >
+            {' '}
+            Contact Details
+            <br />
+          </h2>
+        </header>
+        <div className="py-10 px-10">
+          {tab === 'tab1' ? <ContactForm /> : <div>Contact Details</div>}
+        </div>
+      </article>
+    </main>
+  );
 };
 
-export default Contact
+export default Contact;
+
+/*
+
+I want to have a simple contact form that sends an email upon form submission
+It should have the following fields: email, subject line, message
+possibly also a captcha to avoid spam.
+
+*/
---
+++ components/ContactPage/ContactForm.tsx
@@ -0,0 +1,149 @@
+'use client';
+
+import { useForm, SubmitHandler } from 'react-hook-form';
+import FormError from './FormError';
+import Link from 'next/link';
+
+type FormInputs = {
+  email: string;
+  subjectLine: string;
+  message: string;
+};
+
+const ContactForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitSuccessful },
+    reset,
+  } = useForm<FormInputs>({
+    defaultValues: {
+      email: '',
+      subjectLine: '',
+      message: '',
+    },
+  });
+
+  const onSubmit: SubmitHandler<FormInputs> = (data) => {
+    console.log(data);
+    // reset();
+  };
+
+  const labelClass = 'flex flex-col font-bold gap-1';
+  const inputClass =
+    'border rounded-md dark:bg-black bg-white dark:text-white font-normal pl-2 py-2 outline-none focus:border-2 text-sm sm:text-xl';
+
+  console.log(isSubmitSuccessful);
+
+  return (
+    <>
+      {isSubmitSuccessful ? (
+        <article className="flex flex-col gap-4 text-xl">
+          <h1 className="dark:text-shadow-dark text-shadow-light text-2xl font-bold">
+            Your message was sent successfully!
+          </h1>
+          <p>
+            Thanks for reaching out. I will be in touch as soon as possible to
+            discuss next steps.
+          </p>
+          <div className="flex flex-row gap-6">
+            <Link
+              href="/"
+              className="hover:text-blue dark:hover:text-green mt-4 cursor-pointer rounded-full border px-4 py-2 text-xl shadow-md shadow-gray-600 hover:bg-white dark:shadow-gray-400"
+            >
+              Take Me Home
+            </Link>
+            <Link
+              href="/contact"
+              className="hover:text-blue dark:hover:text-green mt-4 cursor-pointer rounded-full border px-4 py-2 text-xl shadow-md shadow-gray-600 hover:bg-white dark:shadow-gray-400"
+            >
+              Send Another Message
+            </Link>
+          </div>
+        </article>
+      ) : (
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex w-full flex-col gap-4 text-xl"
+        >
+          <header>
+            <p className='font-medium mb-4'>I&apos;m always eager to connect and discuss new opportunities. Please get in touch using the form below. </p>
+          </header>
+          <label className={labelClass}>
+            Email
+            <input
+              type="text"
+              className={inputClass}
+              placeholder="Enter your email address..."
+              {...register('email', {
+                required: { value: true, message: 'Email is required' },
+                minLength: {
+                  value: 5,
+                  message: 'Email addresses must be at least 5 characters long',
+                },
+                maxLength: {
+                  value: 254,
+                  message:
+                    'Email addresses cannot be longer than 254 characters',
+                },
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                  message: 'Please enter a valid email address',
+                },
+              })}
+            />
+            <FormError message={errors.email?.message} />
+          </label>
+          <label className={labelClass}>
+            Subject Line
+            <input
+              type="text"
+              className={inputClass}
+              placeholder="Enter the subject line..."
+              {...register('subjectLine', {
+                required: { value: true, message: 'Subject Line is required' },
+                minLength: {
+                  value: 3,
+                  message: 'Subject Lines must be at least 3 characters long',
+                },
+                maxLength: {
+                  value: 100,
+                  message: 'Subject Lines cannot be longer than 100 characters',
+                },
+              })}
+            />
+            <FormError message={errors.subjectLine?.message} />
+          </label>
+          <label className={labelClass}>
+            Message
+            <textarea
+              className={inputClass}
+              placeholder="Type your message here..."
+              rows={10}
+              {...register('message', {
+                required: { value: true, message: 'Message is required' },
+                minLength: {
+                  value: 20,
+                  message: 'Message must be at least 20 characters long',
+                },
+                maxLength: {
+                  value: 2000,
+                  message: 'Message cannot be longer than 2,000 characters',
+                },
+              })}
+            />
+            <FormError message={errors.message?.message} />
+          </label>
+          <button
+            type="submit"
+            className="hover:text-blue dark:hover:text-green mt-4 cursor-pointer rounded-full border px-4 py-2 text-xl shadow-md shadow-gray-600 hover:bg-white dark:shadow-gray-400"
+          >
+            Send Message
+          </button>
+        </form>
+      )}
+    </>
+  );
+};
+
+export default ContactForm;

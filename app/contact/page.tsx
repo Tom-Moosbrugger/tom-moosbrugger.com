b7b3fc9154d66053388067f472e1879a90a78df6
tom-moosbrugger <tamoosbrugger@gmail.com>
2025-06-13 15:22:53 -0700

    adjusted styling

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -11,25 +11,25 @@ const Contact = () => {
 
   return (
     <main className="flex justify-center px-20 py-10">
-      <article className="rounded-lg border w-full min-w-sm max-w-4xl bg-blue dark:bg-black">
+      <article className="bg-blue w-full max-w-4xl min-w-sm rounded-lg border dark:bg-black">
         <header className="flex flex-row items-center">
           <h2
             onClick={() => setTab('tab1')}
-            className={`text-shadow-light box-content py-3 flex flex-1 cursor-pointer items-center justify-center rounded-tl-lg text-xl font-bold sm:text-3xl ${tab === 'tab1' ? '' : 'bg-lightblue border-r border-b opacity-30'}`}
+            className={`text-shadow-light box-content flex flex-1 cursor-pointer items-center justify-center rounded-tl-lg pt-4 pb-2 text-xl font-bold sm:text-3xl ${tab === 'tab1' ? '' : 'bg-lightblue border-r border-b opacity-30 dark:bg-gray-400'}`}
           >
             Contact Form
             <br />
           </h2>
           <h2
             onClick={() => setTab('tab2')}
-            className={`text-shadow-light box-content py-3 flex flex-1 cursor-pointer items-center justify-center rounded-tr-lg text-xl font-bold sm:text-3xl ${tab === 'tab2' ? '' : 'bg-lightblue border-b border-l opacity-30'}`}
+            className={`text-shadow-light box-content flex flex-1 cursor-pointer items-center justify-center rounded-tr-lg pt-4 pb-2 text-xl font-bold sm:text-3xl ${tab === 'tab2' ? '' : 'bg-lightblue border-b border-l opacity-30 dark:bg-gray-400'}`}
           >
             {' '}
             Contact Details
             <br />
           </h2>
         </header>
-        <div className="py-10 px-10">
+        <div className="px-10 py-10">
           {tab === 'tab1' ? <ContactForm /> : <div>Contact Details</div>}
         </div>
       </article>

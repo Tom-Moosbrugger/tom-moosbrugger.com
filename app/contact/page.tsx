d7611cb4ddf7e946eb0b5da0df000e61c4b0e639
tom-moosbrugger <tamoosbrugger@gmail.com>
2025-06-13 15:33:09 -0700

    created shared variable for tab headers

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,13 +1,13 @@
 'use client';
 
 import ContactForm from '@/components/ContactPage/ContactForm';
-import ComponentElement from '@/components/Elements/ComponentElement';
 import { useState } from 'react';
 
 const Contact = () => {
   const [tab, setTab] = useState('tab1');
 
-  const tabClass = 'border bg-';
+  const baseHeaderClass =
+    'text-shadow-light box-content flex flex-1 cursor-pointer items-center justify-center pt-4 pb-2 text-xl font-bold md:text-3xl';
 
   return (
     <main className="flex justify-center px-20 py-10">
@@ -15,14 +15,14 @@ const Contact = () => {
         <header className="flex flex-row items-center">
           <h2
             onClick={() => setTab('tab1')}
-            className={`text-shadow-light box-content flex flex-1 cursor-pointer items-center justify-center rounded-tl-lg pt-4 pb-2 text-xl font-bold md:text-3xl ${tab === 'tab1' ? '' : 'bg-lightblue border-r border-b opacity-30 dark:bg-gray-400'}`}
+            className={`${baseHeaderClass} rounded-tl-lg ${tab === 'tab1' ? '' : 'bg-lightblue border-r border-b opacity-30 dark:bg-gray-400'}`}
           >
             Contact Form
             <br />
           </h2>
           <h2
             onClick={() => setTab('tab2')}
-            className={`text-shadow-light box-content flex flex-1 cursor-pointer items-center justify-center rounded-tr-lg pt-4 pb-2 text-xl font-bold md:text-3xl ${tab === 'tab2' ? '' : 'bg-lightblue border-b border-l opacity-30 dark:bg-gray-400'}`}
+            className={`${baseHeaderClass} rounded-tr-lg ${tab === 'tab2' ? '' : 'bg-lightblue border-b border-l opacity-30 dark:bg-gray-400'}`}
           >
             {' '}
             Contact Details

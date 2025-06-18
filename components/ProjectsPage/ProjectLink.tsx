2104ad0c2db3ba58f777fdd3efd5ad111f85dd5d
tom-moosbrugger <tamoosbrugger@gmail.com>
2025-06-17 18:20:37 -0700

    adjusted project link min-w

--- components/ProjectsPage/ProjectLink.tsx
+++ components/ProjectsPage/ProjectLink.tsx
@@ -7,7 +7,7 @@ const ProjectLink = ({ url, linkText }: ProjectLinkProps) => {
   return (
     <a
       href={url}
-      className="border-blue dark:border-green hover:text-blue dark:hover:text-green min-w-36 cursor-pointer border-b-2 border-l-2 px-3 py-1 text-center text-base hover:border-2 sm:text-xl md:min-w-44"
+      className="border-blue dark:border-green hover:text-blue dark:hover:text-green min-w-30 cursor-pointer border-b-2 border-l-2 px-3 py-1 text-center text-base hover:border-2 sm:text-xl md:min-w-44"
       target="_blank"
       referrerPolicy="no-referrer"
     >
